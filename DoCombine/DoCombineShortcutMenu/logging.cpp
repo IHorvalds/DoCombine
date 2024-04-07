05cabf8bd85a59e12bb0eed292e51b8d287100c3
Imod Horvalds <ihorvalds@outlook.com>
2024-04-07 20:42:33 +0100

    Fix crash due to being unable to create and open logs path

--- DoCombine/DoCombineShortcutMenu/logging.cpp
+++ DoCombine/DoCombineShortcutMenu/logging.cpp
@@ -3,8 +3,10 @@
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/win_eventlog_sink.h>
 #include <spdlog/fmt/ranges.h>
 #include <filesystem>
+#include <ShlObj.h>
 #include "paths.h"
 
 extern HINSTANCE g_hInst_doCombineExt;
@@ -28,10 +30,26 @@ spdlog::logger& get_default_logger()
     auto pExistingLogger = spdlog::get(::s_module_name);
     if (!pExistingLogger)
     {
-        auto sLogsDir = Paths::get_module_folderpatha(g_hInst_doCombineExt) + "\\..\\logs";
-        if (!std::filesystem::exists(sLogsDir) && !std::filesystem::create_directory(sLogsDir))
+        static std::string    sLogsDir = "";
+        static std::once_flag folder_path_flag;
+        std::call_once(folder_path_flag, []() {
+            auto sModulePath = Paths::get_module_folderpatha(g_hInst_doCombineExt);
+            if (sModulePath.find("C:\\Program Files") != std::string::npos)
+            {
+                char szPath[MAX_PATH];
+                SHGetFolderPathA(NULL, CSIDL_LOCAL_APPDATA, NULL, 0, szPath);
+                sModulePath = std::string(szPath) + "\\TaylorLabs\\DoCombine";
+            }
+
+            sLogsDir = sModulePath + "\\logs";
+        });
+
+        std::error_code ec;
+        if (!std::filesystem::exists(sLogsDir, ec) && !std::filesystem::create_directories(sLogsDir, ec))
         {
-            spdlog::error("Failed to create logs directory");
+            spdlog::default_logger()->sinks().push_back(
+                std::make_shared<spdlog::sinks::win_eventlog_sink_mt>(s_module_name));
+            spdlog::error("Failed to create logs directory, using default logger instead. Error code: {}", ec.value());
             pExistingLogger = spdlog::default_logger();
         }
         else
